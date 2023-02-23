7278ee4aca6d6b727b61eaa09829998d136b0b4f
brightigiemokha <brightigiemokha@yahoo.com>
2023-02-23 22:43:55 +0000

    add corrections and read me

--- script.js
+++ script.js
@@ -1,49 +1,84 @@
 // changing background if correct option picked
 const correct1 = document.getElementById('correct1');
-
 correct1.addEventListener('click', function onClick(event) {
   document.body.style.backgroundColor = 'green';
 });
-const wrong = document.getElementById('wrong');
 
-wrong.addEventListener('click', function onClick(event) {
-  document.body.style.backgroundColor = 'red';
-});
+
 // displaying the first block of question
 document.getElementsByClassName('container')[0].style.display = 'block';
+
 
 
 //DEFINING THE NEXT FUNCTION
 function next(id) {
     document.getElementsByClassName('container')[id-1].style.display = "none"; //make them appear one after another.
     document.getElementsByClassName('container')[id].style.display = "block";
+    document.body.style.backgroundColor = '#2e4376';  
 }
+function correct() {
+  document.getElementById("correct1").innerHTML = body.correct;
+}
+
 //COMPLETION
 function result() {
     var score = 0;
     if (document.getElementById('correct1').checked) {
         score++;
+    } else {
+      document.body.style.backgroundColor = 'red';
     }
 
     if (document.getElementById('correct2').checked) {
         score++;
+    } else {
+      document.body.style.backgroundColor = 'red';
     }
 
     if (document.getElementById('correct3').checked) {
         score++;
-    }
+    } else {
+      document.body.style.backgroundColor = 'red';
+    } 
 
     if (document.getElementById('correct4').checked) {
         score++;
+    } else {
+      document.body.style.backgroundColor = 'red';
     }
-    alert('YOU SCORED: '+ score);
+    if (document.getElementById('correct5').checked) {
+      score++;
+  } else {
+    document.body.style.backgroundColor = 'red';
+  }
+
+  if (document.getElementById('correct6').checked) {
+      score++;
+  } else {
+    document.body.style.backgroundColor = 'red';
+  }
+
+  if (document.getElementById('correct7').checked) {
+    score++;
+} else {
+  document.body.style.backgroundColor = 'red';
 }
 
-function myFunction() {
-    var element = document.getElementById("correct1");
-    element.classList.toggle("body.correct");
-  };
-function myFunction() {
-    var element = document.getElementById("wrong");
-    element.classList.toggle("body.wrong");
-  };
+if (document.getElementById('correct8').checked) {
+    score++;
+} else {
+  document.body.style.backgroundColor = 'red';
+}
+if (document.getElementById('correct9').checked) {
+  score++;
+} else {
+  document.body.style.backgroundColor = 'red';
+}
+
+if (document.getElementById('correct10').checked) {
+  score++;
+} else {
+  document.body.style.backgroundColor = 'red';
+}
+    alert('YOU SCORED: '+ score);
+}
