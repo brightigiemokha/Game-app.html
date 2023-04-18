63afc01047ea0ddb236a565cb392d27a31f79930
brightigiemokha <brightigiemokha@yahoo.com>
2023-04-18 19:56:56 +0000

    preventing multiple scores on 1 q

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let alreadyClicked = false; // to prevent multiple clicks on the same question
+let totalQuestions = 0; // to keep track of total questions
 // changing background if correct option picked
 
 // displaying the first block of question
@@ -7,9 +9,13 @@ document.getElementsByClassName('container')[0].style.display = 'block';
 
 //DEFINING THE NEXT FUNCTION
 function next(id) {
+    if (!alreadyClicked) {
+        return; //if alreadyClicked is false, then return
+    }
     document.getElementsByClassName('container')[id-1].style.display = "none"; //make them appear one after another.
     document.getElementsByClassName('container')[id].style.display = "block"; 
     document.body.style.backgroundColor = 'lightblue';
+    alreadyClicked = false; //resetting the value of alreadyClicked
 }
 
 let score = 0;
