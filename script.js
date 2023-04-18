91f5b710d723b066fe2d8262443808a6222bb2ae
brightigiemokha <brightigiemokha@yahoo.com>
2023-04-18 19:49:07 +0000

    added score-board

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ document.getElementsByClassName('container')[0].style.display = 'block';
 function next(id) {
     document.getElementsByClassName('container')[id-1].style.display = "none"; //make them appear one after another.
     document.getElementsByClassName('container')[id].style.display = "block"; 
-    document.body.style.backgroundColor = 'blue';
+    document.body.style.backgroundColor = 'lightblue';
 }
 
 let score = 0;
@@ -17,12 +17,12 @@ function result(e) {
     let children = Array.from(e.parentElement.children);
     children.every( element => {
         if (element.id == "correct" && element.checked){
-            document.body.style.backgroundColor = 'green';
+            document.body.style.backgroundColor = 'lightgreen';
             score++;
             console.log(score);
             return false;
         }else{
-            document.body.style.backgroundColor = 'red';
+            document.body.style.backgroundColor = '#facdd4';
             return true;
         }
     })
